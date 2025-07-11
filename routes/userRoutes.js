723ab63629790c79e179502cb9268f196aba576a
Tuhin Al Rakib <eng.tuhin77@gmail.com>
2025-07-11 20:46:33 +0600

    users api edit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,12 +12,22 @@ router.post("/", async(req,res)=>{
 router.get("/", async (req, res) => {
   try {
     const db = req.app.locals.db;
-    const users = await db.collection("users").find().toArray();
-    res.json(users);
+    const email = req.query.email; // <-- changed from req.params to req.query
+    const usercollection = db.collection("users");
+
+    if (email) {
+      const query = { email: email };
+      const result = await usercollection.findOne(query);
+      return res.send(result);
+    }
+
+    const users = await usercollection.find().toArray();
+    res.send(users);
   } catch (error) {
     res.status(500).json({ message: "Failed to fetch users", error });
   }
 });
+
 
 
 module.exports =  router 
